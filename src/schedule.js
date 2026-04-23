738e822d2b9082b7e62fc578a649426b058f973a
Nikolay Baranov <113097370+nikolay-a-baranov@users.noreply.github.com>
2026-04-23 14:09:58 +0200

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -1,44 +1,42 @@
 (() => {
+  const hour = 8;
+  const side = hour === 7 ? 'left' : 'right';
+
   const query = (selector) => document.querySelector(selector);
+
+  query('.edit-visibility').click();
+  query('#visibility-radio-public').checked = true;
+  query(`input[name="sticky"][value="${side}"]`).checked = true;
+  query('.save-post-visibility').click();
+
   const setDate = (selector, value) => {
     const field = query(selector);
     field.value = value;
-    field.dispatchEvent(new Event("change", { bubbles: true }));
+    field.dispatchEvent(new Event('change', { bubbles: true }));
   };
-  const currentDate = new Date();
-  const publishDate = new Date(currentDate);
-  if (currentDate.getHours() >= 8) {
-    publishDate.setDate(publishDate.getDate() + 1);
-  }
-  publishDate.setHours(8, 0, 0, 0);
-  const pad = (value) => String(value).padStart(2, "0");
-  
-  query(".edit-visibility").click();
-  query("#visibility-radio-public").checked = true;
-  query('input[name="sticky"][value="right"]').checked = true;
-  query(".save-post-visibility").click();
+  const date = new Date();
+  if (date.getHours() >= hour) date.setDate(date.getDate() + 1);
+  date.setHours(hour, 0, 0, 0);
+  const pad = (value) => String(value).padStart(2, '0');
+  query('.edit-timestamp').click();
+  setDate('#mm', pad(date.getMonth() + 1));
+  setDate('#jj', pad(date.getDate()));
+  setDate('#aa', String(date.getFullYear()));
+  setDate('#hh', pad(hour));
+  setDate('#mn', '00');
+  query('.save-timestamp').click();
 
-  query(".edit-timestamp").click();
-  setDate("#mm", pad(publishDate.getMonth() + 1));
-  setDate("#jj", pad(publishDate.getDate()));
-  setDate("#aa", String(publishDate.getFullYear()));
-  setDate("#hh", "08");
-  setDate("#mn", "00");
-  query(".save-timestamp").click();
+  query('#new-tag-post_tag').value = 'Onliner';
+  query('#post_tag .tagadd').click();
 
-  query("#new-tag-post_tag").value = "Onliner";
-  query("#post_tag .tagadd").click();
+  const layout = [...document.querySelectorAll('select')]
+    .find(select => [...select.options].some(option => option.value === 'longread'));
+  const isLongread = layout && layout.value === 'longread';
+  if (!isLongread && confirm('Не лонгрид! Меняем?')) {
+    layout.value = 'longread';
+    layout.dispatchEvent(new Event('change', { bubbles: true }));
+  }
 
-  const layoutSelect = [...document.querySelectorAll("select")].find((select) =>
-    [...select.options].some((option) => option.value === "longread"),
-  );
-  const isLongread = layoutSelect && layoutSelect.value === "longread";
-  const hasThumbnail = !!query("#postimagediv #set-post-thumbnail img");
-  if (!isLongread && confirm("Не лонгрид! Меняем?")) {
-    layoutSelect.value = "longread";
-    layoutSelect.dispatchEvent(new Event("change", { bubbles: true }));
-  }
-  if (!hasThumbnail) {
-    alert("Минус мини");
-  }
+  const hasThumbnail = !!query('#postimagediv #set-post-thumbnail img');
+  if (!hasThumbnail) alert('Минус мини');
 })();
