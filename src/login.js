b13eb5d8760d010d0d19d247dc0b65cf581432e9
Nikolay Baranov <113097370+nikolay-a-baranov@users.noreply.github.com>
2026-04-24 07:44:06 +0200

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,1 +1,9 @@
-
+// icon: 🔐
+// name: login
+(()=>{
+  document.querySelector('#user_login').value='login';
+  document.querySelector('#user_pass').value='password';
+  document.querySelector('#rememberme').checked=true;
+  document.querySelector("input[name='redirect_to']").value=location.origin+'/wp-admin/edit.php?';
+  document.querySelector('#wp-submit').click();
+})();
