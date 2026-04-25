c86c9ae060138364c6c591db8d5cb05fadf56278
Nikolay Baranov <113097370+nikolay-a-baranov@users.noreply.github.com>
2026-04-25 12:34:13 +0200

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,9 +1,14 @@
 // icon: 🔐
 // name: login
 (()=>{
-  document.querySelector('#user_login').value='login';
-  document.querySelector('#user_pass').value='password';
+  const params=new URLSearchParams(location.search);
+  const post=params.get('post');
+  const redirect=post
+    ?location.origin+'/wp-admin/post.php?post='+post+'&action=edit'
+    :location.origin+'/wp-admin/edit.php?';
+  document.querySelector('#user_login').value='baranov';
+  document.querySelector('#user_pass').value='PASTE_PASSWORD_HERE';
   document.querySelector('#rememberme').checked=true;
-  document.querySelector("input[name='redirect_to']").value=location.origin+'/wp-admin/edit.php?';
+  document.querySelector("input[name='redirect_to']").value=redirect;
   document.querySelector('#wp-submit').click();
 })();
